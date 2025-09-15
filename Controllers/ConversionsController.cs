9267096ec7ddfc9ccb0828fe97ffe596c6987b71
RockBreaker98 <firasathhussaini4@gmail.com>
2025-09-14 19:55:46 -0500

    update changes

---
+++ Controllers/ConversionsController.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MetricSystem.Models;
+
+namespace MetricSystem.Controllers
+{
+    public class ConversionsController : Controller
+    {
+        // GET: /Conversions/Fahrenheit
+        [HttpGet]
+        public IActionResult Fahrenheit()
+        {
+            return View();
+        }
+
+        // POST: /Conversions/Fahrenheit
+        [HttpPost]
+        public IActionResult Fahrenheit(double? fahrenheit)
+        {
+            if (fahrenheit == null)
+            {
+                ViewBag.Error = "Please enter a Fahrenheit value.";
+                return View();
+            }
+
+            double celsius = (fahrenheit.Value - 32) * 5 / 9;
+            ViewBag.Result = $"{celsius:F2}";
+            ViewBag.Fahrenheit = fahrenheit;
+            return View();
+        }
+
+        // GET: /Conversions/Inch
+        public IActionResult Inch()
+        {
+            return View();
+        }
+
+        // GET: /Conversions/Pint
+        public IActionResult Pint()
+        {
+            return View();
+        }
+
+        // GET: /Conversions/Yard
+        public IActionResult Yard()
+        {
+            return View();
+        }
+    }
+}
